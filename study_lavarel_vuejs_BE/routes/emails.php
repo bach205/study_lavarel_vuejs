c518c3dadcc9eab804bc791324052058e3423777
Huy Bach <dhbach13@gmail.com>
2025-09-14 19:04:43 +0700

    fix: add resend function to emails routes

--- study_lavarel_vuejs_BE/routes/emails.php
+++ study_lavarel_vuejs_BE/routes/emails.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmailsController;
 
-Route::prefix('emails')->group(function(){
-    Route::get("/",[EmailsController::class,"index"]);
-    Route::post("/",[EmailsController::class,"store"]);
-    Route::get("/{id}",[EmailsController::class,"show"]);
-    Route::put("/{id}",[EmailsController::class,"update"]);
-    Route::delete("/{id}",[EmailsController::class,"destroy"]);
+Route::prefix('emails')->group(function () {
+    Route::get("/", [EmailsController::class, "index"]);
+    Route::post("/", [EmailsController::class, "store"]);
+    Route::get("/{id}", [EmailsController::class, "show"]);
+    Route::put("/{id}", [EmailsController::class, "update"]);
+    Route::delete("/{id}", [EmailsController::class, "destroy"]);
+    Route::patch("/resend/{id}", [EmailsController::class, "resendEmail"]);
 });
